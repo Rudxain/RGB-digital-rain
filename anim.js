dc528e2cd2fee48d0c505cc12597174e83c3764e
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-05-17 02:57:11 -0400

    Add files via upload

--- anim.js
+++ anim.js
@@ -1,33 +1,43 @@
 'use strict';
 //setup and initialization
-const canvas = document.getElementById('c'), ctx = canvas.getContext('2d', {alpha: false}),
+const canvas = document.getElementById('c'),
+	ctx = canvas.getContext('2d', {alpha: false, desynchronized: true}),
 	w = canvas.width = document.body.offsetWidth,
 	h = canvas.height = document.body.offsetHeight,
 	space = 24, //pixel spacing between chars
-	font = space + 'px monospace',
-	colors = ['f00', 'ff0', '0f0', '0ff', '00f', 'f0f'],//red, yellow, green, cyan, blue, magenta
-	//should these 2 stay hardcoded?
+	//red, yellow, green, cyan, blue, magenta
+	colors = ['f00', 'ff0', '0f0', '0ff', '00f', 'f0f'],
 	charset = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
-	columns = Math.floor(w / space) + 1,
-	y_pos = Array(columns).fill(0);
+	columns = new Uint32Array(w / space + 1),
+	FPS_to_ms = f => 1000 / f,
+	randRange = (min, max) => Math.random() * (max - min) + +min
 
-//set BG to pure black
-ctx.fillStyle = '#000'; ctx.fillRect(0, 0, w, h)
-
-let color_i = 0
-const newFrame = function()
-{
-	ctx.fillStyle = '#0001'; ctx.fillRect(0, 0, w, h);
-	ctx.fillStyle = '#' + colors[color_i++]; color_i %= colors.length; ctx.font = font;
-	y_pos.forEach((y, i) =>
-	{
-		const x = i * space;
-		ctx.fillText(charset[Math.random() * charset.length | 0], x, y);
-		//since the offset and multiplier are arbitrary,
-		//we have the freedom to use powers of 2 for better performance
-		y_pos[i] = y > Math.random() * (1 << 14) + 0x80 ? 0 : y + space;
-	});
+let color_i = 0, start
+const baseFrame = () => {
+	ctx.fillStyle = '#' + colors[color_i++]; ctx.font = space + 'px monospace'
+	for (let i = 0, x = 0; i < columns.length; i++, x += space) {
+		const y = columns[i]
+		//draw a char
+		ctx.fillText(charset[randRange(0, charset.length) | 0], x, y)
+		//since the range is arbitrary, we have freedom to use powers of 2 for performance
+		columns[i] = y > (randRange(1 << 7, 1 << 14) >>> 0) ? 0 : y + space
+	}
 },
-	FPS_to_ms = FPS => 1000 / FPS;
+	nextFrame = now => {
+		if (now - start > FPS_to_ms(30)) {
+			//global dimming, make the trail disappear gradually
+			ctx.fillStyle = '#0001'; ctx.fillRect(0, 0, w, h)
+			color_i %= colors.length
+			baseFrame()
+			start = now
+		}
+		requestAnimationFrame(nextFrame)
+	}
 
-setInterval(newFrame, FPS_to_ms(30))
+requestAnimationFrame(now => {
+	//set BG to pure black
+	ctx.fillStyle = '#000'; ctx.fillRect(0, 0, w, h)
+	baseFrame()
+	start = now
+})
+requestAnimationFrame(nextFrame)
