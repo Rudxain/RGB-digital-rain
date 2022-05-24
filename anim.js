cd89d56706badc004e86e4a103132b9e30b3e070
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-05-23 20:39:17 -0400

    Update anim.js

--- anim.js
+++ anim.js
@@ -5,7 +5,7 @@ const canvas = document.getElementById('c'),
 	w = canvas.width = document.body.offsetWidth,
 	h = canvas.height = document.body.offsetHeight,
 	space = 24, //pixel spacing between chars
-	//red, yellow, green, cyan, blue, magenta
+	//primary and secondary colors
 	colors = ['f00', 'ff0', '0f0', '0ff', '00f', 'f0f'],
 	charset = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
 	columns = new Uint32Array(Math.ceil(w / space)),
