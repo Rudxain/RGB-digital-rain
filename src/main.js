dbba0544ca91e053b956440a0a3386206abb332f
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-08-27 16:33:54 -0400

    Format with VScode

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 'use strict'
-const RAF = requestAnimationFrame , DOC = document
+const RAF = requestAnimationFrame, DOC = document
 
 /**@type {HTMLCanvasElement}*/
 const canv = DOC.getElementById('c')
 
 /**@type {CanvasRenderingContext2D}*/
-const ctx = canv.getContext( '2d', {alpha:false , desynchronized:true} )
+const ctx = canv.getContext('2d', { alpha: false, desynchronized: true })
 
 /**
 list of y values (px, not grid) of last drawn chars
@@ -24,20 +24,20 @@ let is_dark = !light_query?.matches
 
 const settings = {
 	//🌈RYGCBM
-	colors : ['f00','ff0','0f0','0ff','00f','f0f'],
+	colors: ['f00', 'ff0', '0f0', '0ff', '00f', 'f0f'],
 	//not using `Intl.Segmenter`, because grapheme clusters can be rendered at *ANY* size.
 	//supporting code-points instead of code-units is easier and less buggy.
-	charset : [...'!?"\'`#$%&()[]{}*+-,./\\|:;<=>@^_~0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'],
-	speed_Hz : 24,//should only affect `draw_chars`, no-op for dimming
-	grid_px : 32,//grid size
-	min_y : 6, max_y: 14,
-	dim_factor : 1 * (is_dark ? 1 : -1), //dimming coefficient
-	resize_delay_ms : 1500
+	charset: [...'!?"\'`#$%&()[]{}*+-,./\\|:;<=>@^_~0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'],
+	speed_Hz: 24,//should only affect `draw_chars`, no-op for dimming
+	grid_px: 32,//grid size
+	min_y: 6, max_y: 14,
+	dim_factor: 1 * (is_dark ? 1 : -1), //dimming coefficient
+	resize_delay_ms: 1500
 }
 
 let anim = {};
 
-(()=>{
+(() => {
 	let playing = false
 
 	/**
@@ -45,17 +45,17 @@ let anim = {};
 	*/let it_ID
 
 	anim = {
-		get playing() {return playing},
+		get playing() { return playing },
 		set playing(b) {
-			b=!!b
+			b = !!b
 			const prev = playing
 			playing = b
 
 			if (!prev && b) {
 				const Hz_to_ms = f => 1000 / f
 				//the interval ensures `drawChars` is independent of FPS
-				it_ID = setInterval( draw_chars, Hz_to_ms(settings.speed_Hz) )
-				RAF( do_global_dimming )
+				it_ID = setInterval(draw_chars, Hz_to_ms(settings.speed_Hz))
+				RAF(do_global_dimming)
 			}
 			if (prev && !b)
 				clearInterval(it_ID)
@@ -63,8 +63,7 @@ let anim = {};
 	}
 })()
 
-const resize = ()=>
-{
+const resize = () => {
 	canv.width = DOC.body.clientWidth
 	canv.height = DOC.body.clientHeight
 	//calculate how many columns in the grid are needed to fill the whole canvas
@@ -84,13 +83,12 @@ const resize = ()=>
 	anim.playing = prev //revert to previous play-state if needed
 }
 
-const draw_chars = ()=>
-{
-	const {colors, grid_px, charset} = settings
+const draw_chars = () => {
+	const { colors, grid_px, charset } = settings
 
 	ctx.font = `bold ${grid_px}px monospace`
 
-	const rand_u32 = (min=0 , max=2**32) => (Math.random() * (max - min) + min)>>>0
+	const rand_u32 = (min = 0, max = 2 ** 32) => (Math.random() * (max - min) + min) >>> 0
 
 	//according to MDN docs, `forEach` seems to be thread-safe here (I guess)
 	height_ls.forEach((y, i) => {
@@ -98,63 +96,62 @@ const draw_chars = ()=>
 
 		ctx.fillStyle = '#' + color
 
-		let rand = rand_u32( 0, charset.length )
+		let rand = rand_u32(0, charset.length)
 		const x = i * grid_px
-		ctx.fillText( charset[rand], x, y )
+		ctx.fillText(charset[rand], x, y)
 
 		//range is arbitrary, we have freedom to use powers of 2, for performance
-		rand = rand_u32( 1 << settings.min_y, 1 << settings.max_y )
+		rand = rand_u32(1 << settings.min_y, 1 << settings.max_y)
 		y = height_ls[i] = y > rand ? 0 : y + grid_px
 		//if column has been reset, pick next color
 		if (!y) color_i_ls[i] = (color_i_ls[i] + 1) % colors.length
 	})
 }
 
-let t=0 //IDK how to make this private/local and static
+let t = 0 //IDK how to make this private/local and static
 
 /**
 AKA "trail fader"
 @param {number} now
 */
-const do_global_dimming = now =>
-{
+const do_global_dimming = now => {
 	if (!anim.playing) return
 
-	let {dim_factor} = settings
+	let { dim_factor } = settings
 	const sgn = Math.sign(dim_factor)
 	dim_factor = Math.abs(dim_factor)
 
-	const dim = Math.round( Math.min((now - t) * dim_factor , 0xff) )
+	const dim = Math.round(Math.min((now - t) * dim_factor, 0xff))
 
 	//performance...
-	if (dim){
+	if (dim) {
 		//convert to u32 and return a B16 str whose max byte length is `B`
-		const hexPad = (x=0, B=4) => (x>>>0).toString(0x10).padStart(B<<1 , '0')
+		const hexPad = (x = 0, B = 4) => (x >>> 0).toString(0x10).padStart(B << 1, '0')
 
-		ctx.fillStyle = `#${sgn < 0 ? 'ffffff' : '000000'}${hexPad( dim,1 )}`
-		ctx.fillRect( 0, 0, canv.width, canv.height )
+		ctx.fillStyle = `#${sgn < 0 ? 'ffffff' : '000000'}${hexPad(dim, 1)}`
+		ctx.fillRect(0, 0, canv.width, canv.height)
 		//...and ensure hi-FPS don't cause `dim` to get stuck as a no-op.
 		t = now
 	}
-	RAF( do_global_dimming )
+	RAF(do_global_dimming)
 }
 
-const main = ()=>{
+const main = () => {
 	resize() //not part of anim, and has some latency, so no RAF
 
 	ctx.fillStyle = '#' + (settings.dim_factor < 0 ? 'fff' : '000')
-	ctx.fillRect( 0, 0, canv.width, canv.height )
+	ctx.fillRect(0, 0, canv.width, canv.height)
 
-	RAF(now => {draw_chars(); t = now}) //minimal latency for 1st frame
+	RAF(now => { draw_chars(); t = now }) //minimal latency for 1st frame
 	anim.playing = true
 
 	/**
 	needed to debounce `resize`
 	@type {undefined|number}
 	*/let tm_ID
-	addEventListener('resize', ()=>{
-		clearTimeout( tm_ID )
-		tm_ID = setTimeout( resize, settings.resize_delay_ms )
+	addEventListener('resize', () => {
+		clearTimeout(tm_ID)
+		tm_ID = setTimeout(resize, settings.resize_delay_ms)
 	})
 
 	light_query?.addEventListener?.('change', e => {
