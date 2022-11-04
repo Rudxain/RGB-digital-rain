b301680a89387e9896318216959b53ec827a281c
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-11-04 04:37:14 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,19 +2,29 @@
 'use strict'
 //global/public, for debugging/testing purposes
 const RGBDR_anim = (() => {
-	const RAF = requestAnimationFrame, DOC = document
+	const DOC = document, RAF = requestAnimationFrame
 
-	/**@type {HTMLCanvasElement}*/
 	const canv = DOC.getElementById('c')
+	if (!(canv instanceof HTMLCanvasElement)) //shut-up TS-check
+		throw new TypeError('bruh, `canv` is not a Canvas')
+
 	const ctx = canv.getContext('2d', { alpha: false, desynchronized: true })
+	if (!(ctx instanceof CanvasRenderingContext2D)) //shut-up TS-check
+		throw new TypeError('bruh, `ctx` is not a Context')
+
+	const light_query = matchMedia?.('(prefers-color-scheme: light)')
+	//dark must act as default, so light is optional
+	let is_dark = !light_query?.matches
 
 	const anim = (() => {
 		let playing = false
 
-		/**@type {undefined|number}*/
-		let it_ID
+		/**
+		`drawChars` interval ID
+		@type {undefined|number}
+		*/let it_ID
 
-		const anim = {
+		const a = {
 			get playing() { return playing },
 			set playing(b) {
 				b = !!b
@@ -29,37 +39,39 @@ const RGBDR_anim = (() => {
 					*/
 					const Hz_to_ms = f => 1000 / f
 					//the interval ensures `drawChars` is independent of FPS
-					it_ID = setInterval(draw_chars, Hz_to_ms(anim.settings.speed_Hz))
+					it_ID = setInterval(draw_chars, Hz_to_ms(a.settings.char_speed_Hz))
 					RAF(full_dimmer)
 				}
 				if (prev && !b)
 					clearInterval(it_ID)
+			},
+			/*
+			This property shouldn't have access to `playing` and `it_ID`.
+			However, to get better type inference, and organized code,
+			it's defined here, instead of outside the IIFE's closure.
+			*/
+			settings: {
+				/** `Array` of CSS hex colors */
+				colors: ['f00','ff0','0f0','0ff','00f','f0f'],//🌈RYGCBM
+				/** character-set/alphabet */
+				charset:
+					'0123456789' +
+					'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+					'abcdefghijklmnopqrstuvwxyz' +
+					'!?"\'`#$%&()[]{}*+-,./\\|:;<=>@^_~',
+				/** `draw_chars` call-frequency */
+				char_speed_Hz: 24,
+				/** grid cell size */
+				grid_px: 0x20,
+				min_y: 6, max_y: 14,
+				/** dimming coefficient */
+				dim_factor: 1 * (is_dark ? 1 : -1),
+				/** miliseconds to debounce until `resize` is called */
+				resize_delay_ms: 1500
 			}
 		}
-		return anim
+		return a
 	})()
-
-	const light_query = matchMedia?.('(prefers-color-scheme: light)')
-	//dark must act as default, so light is optional
-	let is_dark = !light_query?.matches
-
-	anim.settings = {
-		//🌈RYGCBM
-		colors: ['f00','ff0','0f0','0ff','00f','f0f'],
-		charset:
-			'0123456789' +
-			'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
-			'abcdefghijklmnopqrstuvwxyz' +
-			'!?"\'`#$%&()[]{}*+-,./\\|:;<=>@^_~',
-		/** should only affect `draw_chars`, no-op for dimming */
-		speed_Hz: 24,
-		/** grid size */
-		grid_px: 0x20,
-		min_y: 6, max_y: 14,
-		/** dimming coefficient */
-		dim_factor: 1 * (is_dark ? 1 : -1),
-		resize_delay_ms: 1500
-	}
 
 	/**
 	list of y values (px, not grid) of last drawn chars
@@ -72,10 +84,17 @@ const RGBDR_anim = (() => {
 	@type {number[]}
 	*/const color_i_ls = []
 
+	/**
+	Set `canv` dimensions to fill the full viewport.
+
+	Resize `height_ls` accordingly, padding with 0.
+
+	Resize `color_i_ls` accordingly, padding with `index mod colors.length`.
+	*/
 	const resize = () => {
 		canv.width = DOC.body.clientWidth
 		canv.height = DOC.body.clientHeight
-		//calculate how many columns in the grid are needed to fill the whole canvas
+		//calculate how many columns in the grid are necessary to fill the whole canvas
 		const columns = Math.ceil(canv.width / anim.settings.grid_px)
 
 		const prev = anim.playing
@@ -89,16 +108,24 @@ const RGBDR_anim = (() => {
 			color_i_ls.push(color_i_ls.length % anim.settings.colors.length)
 		color_i_ls.length = columns
 
-		anim.playing = prev //revert to previous play-state if needed
+		anim.playing = prev //revert to previous play-state
 	}
 
 	const draw_chars = () => {
-		const { colors, grid_px, charset } = anim.settings
+		const
+			rng = Math.random,
+			{ colors, grid_px, charset } = anim.settings
 
 		ctx.font = `bold ${grid_px}px monospace`
 
-		/** Returns a pseudorandom unsigned 32bit int. */
-		const rand_u32 = (min = 0, max = 2 ** 32) => (Math.random() * (max - min) + min) >>> 0
+		/** Returns a pseudo-random unsigned 32bit int. */
+		const rng_u32 = (min = 0, max = 2 ** 32) => (rng() * (max - min) + min) >>> 0
+
+		/**
+		Get a pseudo-random UTF-16 code-unit from a `string`.
+		@param {string} s
+		*/
+		const rand_CU_pick = s => s[rng() * s.length]
 
 		//according to MDN docs, `forEach` seems to be thread-safe here (I guess)
 		height_ls.forEach((y, i) => {
@@ -106,12 +133,11 @@ const RGBDR_anim = (() => {
 
 			ctx.fillStyle = '#' + color
 
-			let rand = rand_u32(0, charset.length)
 			const x = i * grid_px
-			ctx.fillText(charset[rand], x, y)
+			ctx.fillText(rand_CU_pick(charset), x, y)
 
 			//range is arbitrary, we have freedom to use powers of 2, for performance
-			rand = rand_u32(1 << anim.settings.min_y, 1 << anim.settings.max_y)
+			const rand = rng_u32(1 << anim.settings.min_y, 1 << anim.settings.max_y)
 			y = height_ls[i] = y > rand ? 0 : y + grid_px
 			//if column has been reset, pick next color
 			if (!y) color_i_ls[i] = (color_i_ls[i] + 1) % colors.length
@@ -131,6 +157,17 @@ const RGBDR_anim = (() => {
 		const
 			df = anim.settings.dim_factor, //avoid race condition, and short alias
 			dim = Math.round(Math.min((now - t) * Math.abs(df), 0xff))
+
+		{
+			const is_neg_zero = x => x === 0 && 1 / x == -Infinity
+
+			/**
+			Check if `x` is an unsigned 8bit int. Returns `false` for `BigInt`s.
+			@param {*} x
+			*/
+			const is_u8 = x => typeof x == 'number' && x == (x & 0xff) && !is_neg_zero(x)
+			console.assert(is_u8(dim))
+		}
 
 		//performance...
 		if (dim) {
@@ -157,11 +194,12 @@ const RGBDR_anim = (() => {
 		ctx.fillStyle = anim.settings.dim_factor < 0 ? '#fff' : '#000'
 		ctx.fillRect(0, 0, canv.width, canv.height)
 
-		RAF(now => { draw_chars(); t = now }) //minimal latency for 1st frame
+		//minimal latency for 1st frame
+		RAF(now => { draw_chars(); t = now })
 		anim.playing = true
 
 		/**
-		needed to debounce `resize`
+		timeout ID, necessary to debounce `resize`
 		@type {undefined|number}
 		*/let tm_ID
 		addEventListener('resize', () => {
@@ -174,10 +212,11 @@ const RGBDR_anim = (() => {
 			anim.settings.dim_factor = Math.abs(anim.settings.dim_factor) * (is_dark ? 1 : -1)
 		})
 	}
+	main()
 
-	// @ts-ignore
-	if (typeof require == 'undefined' && typeof WorkerGlobalScope == 'undefined')
-		main()
+	/* const tester = () => {
+
+	} */
 
 	return anim
 })()
